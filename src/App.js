0fe7450227319950315f8f1dc893e576e815277c
koderoom <koderoom.dev@gmail.com>
2021-01-20 12:07:52 +0530

    vaery berif about form elemnts.

--- src/App.js
+++ src/App.js
@@ -19,11 +19,21 @@ function App() {
 function Post() {
   const [title] = useState("Working with Form Input Element");
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   // When the state of Input Element is udpated. At the same
   // time we are updating, the state of React State Variabel.
   // To keep the value in SYNC.
   const updateInput = (e) => setUsername(e.target.value);
+  const updateInputPassword = (e) => setPassword(e.target.value);
+
+  const processLogin = () => {
+    console.log(username, password);
+    // we get the user input
+    // we have to validte this user input
+    // who will validate this. TIER2/LAYER2/BACKEND
+    // we have to make ajax call to the BACKEND SERVER.
+  };
 
   return (
     <div>
@@ -36,6 +46,26 @@ function Post() {
         name="username"
         placeholder="Username"
       />
+
+      <br />
+      <input
+        type="password"
+        value={password}
+        onChange={updateInputPassword}
+        name="username"
+        placeholder="Password"
+      />
+
+      <br />
+      <button onClick={processLogin}>Login</button>
+
+      <br></br>
+
+      <div>Username :: {username}</div>
+
+      <div>Username :: {username}</div>
+
+      <div>Username :: {username}</div>
     </div>
   );
 }
