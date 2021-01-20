303f82a376897d9a0f162bd55203e87a8cfde1d5
koderoom <koderoom.dev@gmail.com>
2021-01-20 12:21:32 +0530

    working with form element and react state.

--- src/App.js
+++ src/App.js
@@ -10,29 +10,20 @@ function App() {
   );
 }
 
-/**
- * <Post /> React also has "state"
- *
- * <div></div>
- * <input></input> :: All the form element  has thier own "state".
- */
 function Post() {
-  const [title] = useState("Working with Form Input Element");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [title] = useState("Dynamic Comment List");
 
-  // When the state of Input Element is udpated. At the same
-  // time we are updating, the state of React State Variabel.
-  // To keep the value in SYNC.
-  const updateInput = (e) => setUsername(e.target.value);
-  const updateInputPassword = (e) => setPassword(e.target.value);
+  const [singleComment, setSingleComment] = useState("");
+  const updateTheSingleComment = (e) => setSingleComment(e.target.value);
 
-  const processLogin = () => {
-    console.log(username, password);
-    // we get the user input
-    // we have to validte this user input
-    // who will validate this. TIER2/LAYER2/BACKEND
-    // we have to make ajax call to the BACKEND SERVER.
+  // Immutable object
+  const [comments, setComments] = useState([]);
+
+  const addComment = () => {
+    // setComments(["Hello Universe", ...comments]);
+    setComments([singleComment, ...comments]);
+
+    setSingleComment("");
   };
 
   return (
@@ -41,31 +32,17 @@ function Post() {
 
       <input
         type="text"
-        value={username}
-        onChange={updateInput}
-        name="username"
-        placeholder="Username"
+        name=""
+        value={singleComment}
+        onChange={updateTheSingleComment}
+        placeholder="Type ur comment..."
       />
+      <button onClick={addComment}>Add Comment</button>
 
-      <br />
-      <input
-        type="password"
-        value={password}
-        onChange={updateInputPassword}
-        name="username"
-        placeholder="Password"
-      />
-
-      <br />
-      <button onClick={processLogin}>Login</button>
-
-      <br></br>
-
-      <div>Username :: {username}</div>
-
-      <div>Username :: {username}</div>
-
-      <div>Username :: {username}</div>
+      <div>Comments {comments.length}</div>
+      {comments.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
     </div>
   );
 }
